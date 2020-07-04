aa8f8d6adc21a795b0184ba9e225afe575c0a8ab
Robert Barnes (DevOps Rob) <rbarnes@hashicorp.com>
2020-07-04 01:48:03 +0100

    updated command structure for nomad to replace name flag with arg

--- cmd/nomadcreate.go
+++ cmd/nomadcreate.go
@@ -1,19 +1,26 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
 )
 
 var nomadCreateCmd = &cobra.Command{
-	Use:     "create",
+	Use:     "create [name]",
 	Short:   "create command creates a context profile",
 	Long:    `create a context profile with the create command.`,
-	Example: `target nomad create --name="example" --endpoint="https://example-nomad.com:8200"`,
+	Example: `target nomad create example --endpoint="https://example-nomad.com:8200"`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			return errors.New("requires a name argument")
+		}
+		return nil
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		// The following code is to test out CLI behaviour
-		fmt.Printf("creating %v target profile with the following values: \n==========================================================\n", nomadname)
+		fmt.Printf("creating %v target profile with the following values: \n==========================================================\n", args[0])
 
 		fmt.Printf("endpoint flag value: %v\n", nomadendpoint)
 
@@ -43,7 +50,6 @@ var nomadCreateCmd = &cobra.Command{
 
 func init() {
 
-	nomadCreateCmd.PersistentFlags().StringVar(&nomadname, "name", "", "set a profile name for this context")
 	nomadCreateCmd.PersistentFlags().StringVar(&nomadendpoint, "endpoint", "", "set target endpoint details. e.g https://example-nomad.com:4646")
 	nomadCreateCmd.PersistentFlags().StringVar(&nomadregion, "region", "", "set the region of the Nomad server to forward commands to")
 	nomadCreateCmd.PersistentFlags().StringVar(&nomadtoken, "token", "", "set the  SecretID of an ACL token to use to authenticate API requests with")
@@ -52,10 +58,6 @@ func init() {
 	nomadCreateCmd.PersistentFlags().StringVar(&nomadcert, "cert", "", "set path to a PEM-encoded client certificate on the local disk")
 	nomadCreateCmd.PersistentFlags().StringVar(&nomadkey, "key", "", "set path to an unencrypted, PEM-encoded private key on disk which corresponds to the matching client certificate")
 	nomadCreateCmd.PersistentFlags().StringVar(&nomadnamespace, "namespace", "", "set Nomad namespace to use for command")
-
-	nomadCreateCmd.MarkPersistentFlagRequired(
-		"name",
-	)
 
 	nomadCreateCmd.MarkPersistentFlagRequired(
 		"endpoint",
--- cmd/nomadupdate.go
+++ cmd/nomadupdate.go
@@ -1,19 +1,26 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
 )
 
 var nomadUpdateCmd = &cobra.Command{
-	Use:     "update",
+	Use:     "update [name]",
 	Short:   "Update an existing context",
 	Long:    `The update command allows you to modify an existing context.`,
-	Example: `target nomad update --name="example" --endpoint="https://example2-nomad.com:8200"`,
+	Example: `target nomad update example --endpoint="https://example2-nomad.com:8200"`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			return errors.New("requires a name argument")
+		}
+		return nil
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		// The following code is to test out CLI behaviour
-		fmt.Printf("updating %v target profile with the following values: \n==========================================================\n", nomadname)
+		fmt.Printf("updating %v target profile with the following values: \n==========================================================\n", args[0])
 
 		if len(nomadendpoint) != 0 {
 			fmt.Printf("endpoint flag new value: %v\n", nomadendpoint)
@@ -43,7 +50,6 @@ var nomadUpdateCmd = &cobra.Command{
 }
 
 func init() {
-	nomadUpdateCmd.PersistentFlags().StringVar(&nomadname, "name", "", "set a profile name for this context")
 	nomadUpdateCmd.PersistentFlags().StringVar(&nomadendpoint, "endpoint", "", "set target endpoint details. e.g https://example-nomad.com:4646")
 	nomadUpdateCmd.PersistentFlags().StringVar(&nomadregion, "region", "", "set the region of the Nomad server to forward commands to")
 	nomadUpdateCmd.PersistentFlags().StringVar(&nomadtoken, "token", "", "set the  SecretID of an ACL token to use to authenticate API requests with")
@@ -52,9 +58,5 @@ func init() {
 	nomadUpdateCmd.PersistentFlags().StringVar(&nomadcert, "cert", "", "set path to a PEM-encoded client certificate on the local disk")
 	nomadUpdateCmd.PersistentFlags().StringVar(&nomadkey, "key", "", "set path to an unencrypted, PEM-encoded private key on disk which corresponds to the matching client certificate")
 	nomadUpdateCmd.PersistentFlags().StringVar(&nomadnamespace, "namespace", "", "set Nomad namespace to use for command")
-
-	nomadUpdateCmd.MarkPersistentFlagRequired(
-		"nomadname",
-	)
 
 }
