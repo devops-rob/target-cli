6123be2797400196b46a22f4a9619e5d94640206
Robert Barnes (DevOps Rob) <rbarnes@hashicorp.com>
2020-07-04 01:52:04 +0100

    updating rename command with new cli flow

--- cmd/rename.go
+++ cmd/rename.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
@@ -8,23 +9,24 @@ import (
 
 // renameCmd represents the rename command
 var renameCmd = &cobra.Command{
-	Use:     "rename",
-	Short:   "rename a vault conext profile",
-	Long:    `rename a vault context profile using the rename command`,
-	Example: `target vault rename --name="example" --rename="example2"`,
+	Use:     "rename [name]",
+	Short:   "rename a conext profile",
+	Long:    `rename a context profile using the rename command`,
+	Example: `target vault rename example --rename="example2"`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			return errors.New("requires a name argument")
+		}
+		return nil
+	},
+
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("rename called")
 	},
 }
 
 func init() {
-	// vaultCmd.AddCommand(renameCmd)
-	renameCmd.PersistentFlags().StringP("name", "n", "", "name of context to be renamed")
 	renameCmd.PersistentFlags().StringP("rename", "r", "", "set new name for this context")
-
-	renameCmd.MarkPersistentFlagRequired(
-		"name",
-	)
 
 	renameCmd.MarkPersistentFlagRequired(
 		"rename",
